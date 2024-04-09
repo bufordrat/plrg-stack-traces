4e12480e48b3591234e0a2718c7d4999d8d9b149
Matt Teichman <teichman@uchicago.edu>
2024-04-09 11:35:48 -0500

    complete simple example

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,5 +1,3 @@
 let message = "GOODBYE CRUEL WORLD (is underrated)"
 
-(* Local Variables: *)
-(* mode: tuareg *)
-(* End: *)
+module Simple = Simple
--- lib/simple.ml
+++ lib/simple.ml
@@ -4,28 +4,25 @@ type ('a, 'b) result =
 
 let ( >>= ) mx k =
   match mx with
-  | Ok o -> Ok (k o)
-  | err -> err
+  | Ok o -> k o
+  | Error e -> Error e
 
 let pure x = Ok x
 
 let ( let* ) = ( >>= )
 
-let pred_to_kleisli pred msg x =
-  if pred x
-  then Ok x
-  else Error msg
-
 let is_positive n =
-  pred_to_kleisli
-    (fun n -> n > 0)
-    "number should be positive"
-    n
+  if n > 0
+  then Ok n
+  else Error "not positive"
 
 let is_even n =
-  pred_to_kleisli
-    (fun n -> n mod 2 = 0)
-    "number should be even"
-    n
+  if n mod 2 = 0
+  then Ok n
+  else Error "not even"
 
-
+let validate_then_double n =
+  let* _ = is_positive n in
+  let* validated = is_even n in
+  pure (validated * 2)
+               
