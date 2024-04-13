8488649880719ca35a85ba204c41e6eee5e92c4b
Matt Teichman <teichman@uchicago.edu>
2024-04-13 08:36:58 -0500

    still gotta write read_config

--- lib/config.ml
+++ lib/config.ml
@@ -2,5 +2,7 @@ type t = Config_error.t
 
 module R = Etude.Result.Make (Global_error)
 
-(* let read_config =
- *   let* authenticated_username *)
+let read_config =
+  let open R in 
+  let* _ = assert false in
+  assert false
