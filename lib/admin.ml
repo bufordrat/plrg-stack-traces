684e5c3254b0a3b7a5e5217db4286912524a65f4
Matt Teichman <teichman@uchicago.edu>
2024-04-15 19:12:34 -0500

    update first stab example to fit other examples

--- lib/admin.ml
+++ lib/admin.ml
@@ -1,10 +1,12 @@
 module R = Etude.Result.Make (Global_error)
 
-(* let open_admin_panel username =
- *   let open R in
- *   let open Authorization in
- *   let open Session in
- *   let* token = with_error connect username in
- *   if List.mem username sudoers
- *   then assert false
- *   else assert false *)
+let open_admin_panel username =
+  let open R in
+  let open Session in
+  let open Authorization in
+  let open Global_error in
+  let token =
+    let* t = connect username in
+    pure (CreateSession t)
+  in
+  with_error `WrongPrivileges token
