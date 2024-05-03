01b84941f81d528445ff2cca514abeccb571633d
Matt Teichman <teichman@uchicago.edu>
2024-05-02 20:46:40 -0500

    remove extraneous mli-s

--- lib/admin.mli
+++
@@ -1,2 +0,0 @@
-val open_admin_panel :
-  string -> (Session.t, Global_error.t) result
--- lib/config.mli
+++
@@ -1,8 +0,0 @@
-type t = { username : string
-         ; token : Session.token
-         ; message : string }
-
-val read_config :
-  string ->
-  string ->
-  (t, Global_error.t) result
